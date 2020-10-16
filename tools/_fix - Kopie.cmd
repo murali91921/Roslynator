7644b64f51931629773d10f85faa78728616b8ca
Josef Pihrt <josef.pihrt@outlook.com>
2020-10-16 06:04:31 +0200

    x

--- tools/_fix - Kopie.cmd
+++ tools/_fix - Kopie.cmd
@@ -5,17 +5,21 @@ set _msbuildPath="%_visualStudioPath%\MSBuild\Current\Bin"
 
 %_msbuildPath%\msbuild "..\src\CommandLine.sln" /t:Build /p:Configuration=Debug /v:m /m
 
-"..\src\CommandLine\bin\Debug\net48\roslynator" fix "..\src\Core\Core.csproj" ^
+"..\src\CommandLine\bin\Debug\net48\roslynator" fix "..\src\Roslynator.sln" ^
  --msbuild-path %_msbuildPath% ^
- --supported-diagnostics RCS1016 RCS0052 ^
  --projects Core ^
  --analyzer-assemblies ^
   "..\src\Analyzers.CodeFixes\bin\Debug\netstandard2.0\Roslynator.CSharp.Analyzers.dll" ^
   "..\src\Analyzers.CodeFixes\bin\Debug\netstandard2.0\Roslynator.CSharp.Analyzers.CodeFixes.dll" ^
+  "..\src\CodeAnalysis.Analyzers.CodeFixes\bin\Debug\netstandard2.0\Roslynator.CodeAnalysis.Analyzers.dll" ^
+  "..\src\CodeAnalysis.Analyzers.CodeFixes\bin\Debug\netstandard2.0\Roslynator.CodeAnalysis.Analyzers.CodeFixes.dll" ^
   "..\src\Formatting.Analyzers.CodeFixes\bin\Debug\netstandard2.0\Roslynator.Formatting.Analyzers.dll" ^
   "..\src\Formatting.Analyzers.CodeFixes\bin\Debug\netstandard2.0\Roslynator.Formatting.Analyzers.CodeFixes.dll" ^
+ --format ^
  --verbosity d ^
  --file-log "roslynator.log" ^
- --file-log-verbosity diag
+ --file-log-verbosity diag ^
+ --diagnostic-fix-map "RCS1155=Roslynator.RCS1155.OrdinalIgnoreCase" ^
+ --file-banner " Copyright (c) Josef Pihrt. All rights reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information."
 
 pause
